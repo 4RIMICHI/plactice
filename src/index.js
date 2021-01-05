43ab81f5236a2d84954f4d46ee5f1e94dbe7f8e5
4RIMICHI <68262220+4RIMICHI@users.noreply.github.com>
2021-01-05 16:02:47 +0900

    created

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  const inputText = document.getElementById("add-text").Value;
-  alert(inputText);
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  div.appendChild(li);
+  document.getElementById("incomplete").appendChild(div);
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    const completeTarget = completeButton.parentNode;
+    document.getElementById("A").appendChild(completeTarget);
+  });
+  div.appendChild(completeButton);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete").removeChild(deleteTarget);
+  });
+  div.appendChild(deleteButton);
 };
+
+const completeAdd = () => {};
 
 document
   .getElementById("add-button")
